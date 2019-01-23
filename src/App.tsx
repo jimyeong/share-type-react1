4cec827f06f77950b04a4f854c9dbe64336aa06a
jimyeong <idjjm92@gmail.com>
2019-01-23 12:30:56 +0900

    first setup

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+          <div className="test">
+              asd
+          </div>
+      </div>
+    );
+  }
+}
+
+export default App;
